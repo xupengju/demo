685e968255af2d0bb2ea7cfb93afaaa75904d75d
pengju.xu <434431984@qq.com>
2017-06-19 17:14:26 +0800

    去去去

--- src/concurrent/reentrantlock/TestReentrantLock.java
+++ src/concurrent/reentrantlock/TestReentrantLock.java
@@ -8,12 +8,12 @@ class TestReentrantLock {
     public void print(int str) {
         try {
             lock.lock();
-            System.out.println(str + "ªÒµ√");
+            System.out.println(str + "11");
             Thread.sleep((int) (Math.random() * 1000));
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
-            System.out.println(str + " Õ∑≈");
+            System.out.println(str + "222");
             lock.unlock();
         }
     }
