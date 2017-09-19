463941d115145b2fdcf2a0badfd6eb086ab7000a
pengju.xu <434431984@qq.com>
2017-09-19 19:58:32 +0800

    我是注释

--- src/J8/StringTest.java
+++ src/J8/StringTest.java
@@ -3,7 +3,7 @@ package J8;
 /**
  * Created by Milo on 2017/5/28.
  */
-public class StirngTest {
+public class StringTest {
     public static void main(String[] args) {
 
         Something something = new Something();
